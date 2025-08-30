b5efd596d4bf5f6e6795b7b95aa3d6179579bdd2
lancehampton <lance.r.hampton@icloud.com>
2025-08-30 09:56:28 -0400

    add uptime-kuma stack for testing

--- infrastructure/tailscale.tf
+++
@@ -1,7 +0,0 @@
-resource "tailscale_tailnet_key" "pve" {
-  reusable      = true
-  ephemeral     = false
-  preauthorized = true
-  expiry        = 7776000 # 90 days
-  description   = "proxmox-ve-${var.ve_node_name}"
-}
